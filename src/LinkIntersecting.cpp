ab6d0bef3b224e9f4525606885ecb92a1965abe5
zyin <zyin@zyindeMacBook-Pro.local>
2017-03-22 19:35:20 +0800

    add judge two links crossed

--- src/LinkIntersecting.cpp
+++ src/LinkIntersecting.cpp
@@ -11,30 +11,27 @@ void insertNode(Link *&head, Link *&tail, int data);
 void buildLoopLink(Link *&head);
 Link *existLoop(Link *head);
 
+void buildCrossLink(Link *&headA, Link *&headB, Link *&tail);
+
 int main() {
 	Link *head = NULL;
+//判断是否有环
 	buildLoopLink(head);
-
 	Link *loop = existLoop(head);
 	if (NULL != loop) {
-		cout << "Exist loop, value:" << loop->data << endl; // prints !!!Hello World!!!
+		cout << "Exist loop, value:" << loop->data << endl;
+	}
+
+//判断两个链表是否相交，简单场景，假设俩个链表均不带环
+	Link *headA = NULL, *headB = NULL, *tail = NULL;
+	buildCrossLink(headA, headB, tail);
+	tail->next = headA;
+	Link *loopAB = existLoop(headB);
+	if (NULL != loopAB) {
+		cout << "Two list cross, value:" << loopAB->data << endl;
 	}
 
 	return 0;
-}
-
-Link * existLoop(Link *head) {
-	Link * fast, *slow;
-	fast = slow = head;
-	while (fast != NULL && fast->next != NULL) {
-		fast = fast->next->next;
-		slow = slow->next;
-		if (slow == fast) {
-			return slow;
-		}
-	}
-
-	return NULL;
 }
 
 void buildLoopLink(Link *&head) {
@@ -57,3 +54,35 @@ void insertNode(Link *&head, Link *&tail, int data) {
 	}
 	head = node;
 }
+
+Link * existLoop(Link *head) {
+	Link * fast, *slow;
+	fast = slow = head;
+	while (fast != NULL && fast->next != NULL) {
+		fast = fast->next->next;
+		slow = slow->next;
+		if (slow == fast) {
+			return slow;
+		}
+	}
+
+	return NULL;
+}
+
+void buildCrossLink(Link *&headA, Link *&headB, Link *&tail) {
+	Link *tailA = NULL, *tailB = NULL;
+	Link *cross = NULL;
+	for (int i = 1; i <= 10; i++) {
+		insertNode(headA, tailA, i);
+		if (i == 6) {
+			cross = headA;
+		}
+	}
+	tail = tailA;
+
+	for (int i = 11; i <= 15; i++) {
+		insertNode(headB, tailB, i);
+	}
+	tailB->next = cross;
+}
+
